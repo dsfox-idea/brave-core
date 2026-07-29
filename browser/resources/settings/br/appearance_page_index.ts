187ef960a1dba609460cd3a8d218dbcaf86021d7
Emerick Rogul <erogul@brave.com>
2026-07-29 15:57:10 -0400

    [cr152][WIP] Update appearance_page after upstream Lit migration
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/9014775a3bd4fa8dca6e3ad142a55d67ffb947d0
    
    commit 9014775a3bd4fa8dca6e3ad142a55d67ffb947d0
    Author: dpapad <dpapad@chromium.org>
    Date:   Fri Jul 10 14:25:03 2026 -0700
    
        Settings Lit: Migrate appearance_page/, part4.
    
        In this part actually migrating settings-appearance-page to Lit.
    
        Also fixing a bug in PrefService#resetInstanceForTesting() where a stale
        listener of the previous singleton instance was leaked causing stale
        callbacks to be triggered and some tests to fail due to the leaking
        state.
    
        Bug: 393471368
        Change-Id: I297967f9afbd5ee2629ffc44545b65ef02d89dda
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8065711
        Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
        Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
        Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1660535}

--- browser/resources/settings/br/appearance_page_index.ts
+++ browser/resources/settings/br/appearance_page_index.ts
@@ -3,63 +3,10 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-import { html, RegisterPolymerPrototypeModification, RegisterPolymerTemplateModifications } from 'chrome://resources/brave/polymer_overriding.js'
-import { routes } from '../route.js';
-import type { Route } from '../router.js';
-
-// Modify the prototype to handle route changes
-RegisterPolymerPrototypeModification({
-  'settings-appearance-page-index': (prototype) => {
-    prototype.currentRouteChanged = function (newRoute: Route) {
-      const showDefaultViews = () => {
-        this.$.viewManager.switchViews(
-          ['parent', 'tabs', 'sidebar'], 'no-animation', 'no-animation');
-      }
-
-      switch (newRoute) {
-        case routes.BASIC:
-        case routes.APPEARANCE:
-          showDefaultViews();
-          break;
-      }
-    }
-  }
-})
-
-RegisterPolymerTemplateModifications({
-  'settings-appearance-page-index': (template) => {
-    // Support having multiple cards active at the same time.
-    template.prepend(html`
-      <style>
-        cr-view-manager [slot=view]:not(.closing) {
-          position: initial;
-        }
-      </style>
-    `)
-
-    const viewManager = template.querySelector('cr-view-manager')
-    if (!viewManager) {
-      console.error('View manager not found')
-      return
-    }
-
-    // Fonts page is rendered in the content subpage.
-    viewManager.querySelector('#fonts')?.remove()
-
-    // Append tabs and sidebar settings to the appearance page.
-    viewManager.append(html`
-      <settings-brave-appearance-tabs
-        id="tabs"
-        slot="view"
-        prefs="{{prefs}}"
-        in-search-mode="[[inSearchMode_]]">
-      </settings-brave-appearance-tabs>
-
-      <settings-brave-appearance-sidebar
-        id="sidebar"
-        slot="view"
-        prefs="{{prefs}}"
-        in-search-mode="[[inSearchMode_]]">
-      </settings-brave-appearance-sidebar>`)
-  }
-})
+// Registers <settings-brave-appearance-tabs> and
+// <settings-brave-appearance-sidebar>, which are woven into
+// settings-appearance-page-index's template by a lit_mangler override (see
+// chromium_src/chrome/browser/resources/settings/appearance_page/
+// appearance_page_index.html.ts.lit_mangler.ts).
+import '../brave_appearance_page/tabs.js'
+import '../brave_appearance_page/sidebar.js'
